35daa1978189737afb4e0272d5b8d51e20a62055
juniorsundar-tii <junior.sundar@tii.ae>
2023-12-26 15:59:16 +0400

    Updates in launch sequence

--- launch_sim.sh
+++ launch_sim.sh
@@ -6,10 +6,20 @@ source install/local_setup.bash
 
 ros2 run px4_fault_injection drone_controller.py &
 
+sleep 2
+
 ros2 run px4_fault_injection simulation_manager.py &
 
-ros2 run px4_fault_injection drone_state_monitor.py
+sleep 10
 
-# ros2 run px4_fault_injection iteration_runner.py
+ros2 run px4_fault_injection drone_state_monitor.py &
+
+ros2 run px4_fault_injection sensor_recorder_unsync.py &
+
+ros2 run px4_fault_injection mission_manager.py &
+
+ros2 run px4_fault_injection iteration_runner.py &
+
+ros2 run px4_fault_injection fault_manager.py
 
 wait
