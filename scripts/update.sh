f52afb48823c1593cfc48221412a7fb0ed06a1ff
juniorsundar-tii <149654705+juniorsundar-tii@users.noreply.github.com>
2024-01-19 13:08:04 +0400

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,11 +4,11 @@ set -e
 
 cd ~/px4_fault_ws
 
-mv -r ./src/px4_fault_injection ./px4_fault_injection
+mv ./src/px4_fault_injection ./px4_fault_injection
 
 git pull
 
-mv -r ./px4_fault_injection ./src/px4_fault_injection
+mv ./px4_fault_injection ./src/px4_fault_injection
 
 cd ..
 
